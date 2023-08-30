5bd3bdc14c961405d9eef29a0cf6b6d283c8fed5
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-30 12:32:02 -0500

    Update finetune-gpt2.py

--- config/finetune-gpt2.py
+++ config/finetune-gpt2.py
@@ -2,7 +2,7 @@ import time
 
 out_dir = '/content/drive/MyDrive/Model' # for google colab, for regular use, set value to 'out-chat'
 eval_interval = 5
-eval_iters = 50
+eval_iters = 20
 wandb_log = False # feel free to turn on
 wandb_project = 'gpt2finetune'
 wandb_run_name = 'ft-' + str(time.time())
@@ -15,7 +15,7 @@ always_save_checkpoint = False
 
 batch_size = 4
 gradient_accumulation_steps = 32
-max_iters = 50
+max_iters = 20
 
 # finetune at constant LR
 learning_rate = 2e-5
